fa0d949c40fedac5e3bc15ef57c881fbeefffb1a
OlgaWold <oboyantseva.1996@gmail.com>
2024-03-21 14:20:11 +0100

    tavg

--- run-consumer.py
+++ run-consumer.py
@@ -90,8 +90,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
     output_grids = {
         "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         "Precip": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "TempMean": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "globrad": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
+        "Tavg": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
     }
     output_keys = list(output_grids.keys())
 
