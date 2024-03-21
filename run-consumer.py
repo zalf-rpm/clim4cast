268f6f70a00ccf3d1ba6e212a436b7cc243e677d
OlgaWold <oboyantseva.1996@gmail.com>
2024-03-21 14:47:12 +0100

    srad

--- run-consumer.py
+++ run-consumer.py
@@ -91,7 +91,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
         "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         "Precip": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         "Tavg": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Radiation": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
+        "Srad": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
     }
     output_keys = list(output_grids.keys())
 
