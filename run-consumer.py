513a8f9eb7bc5c0ea3519b08855f0c775498248d
OlgaWold <oboyantseva.1996@gmail.com>
2024-03-21 15:10:04 +0100

    globrad

--- run-consumer.py
+++ run-consumer.py
@@ -89,9 +89,9 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
 
     output_grids = {
         "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Precip": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Tavg": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Srad": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
+        #"Precip": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        #"Tavg": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "Globrad": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
     }
     output_keys = list(output_grids.keys())
 
