3d46733c3909ee0403d0a13fa7444c6e5f8fd205
OlgaWold <oboyantseva.1996@gmail.com>
2024-03-21 14:36:12 +0100

    radiation

--- run-consumer.py
+++ run-consumer.py
@@ -90,7 +90,8 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
     output_grids = {
         "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         "Precip": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Tavg": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
+        "Tavg": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "Radiation": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
     }
     output_keys = list(output_grids.keys())
 
