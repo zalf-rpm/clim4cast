95e5d5f78ce21da65623c16274556091e718ce44
OlgaWold <oboyantseva.1996@gmail.com>
2024-06-20 15:17:05 +0200

    new outputs

--- run-consumer.py
+++ run-consumer.py
@@ -89,7 +89,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
 
     output_grids = {
         "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        #"AbBiom": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "AbBiom": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         #"HeatRed": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         #"HeatRed_S1": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         #"HeatRed_S2": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
