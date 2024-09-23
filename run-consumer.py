0d3351505b3d1e63170102d04dac3a3ff138e0c1
OlgaWold <oboyantseva.1996@gmail.com>
2024-09-23 15:31:56 +0200

    whole germany run

--- run-consumer.py
+++ run-consumer.py
@@ -88,8 +88,10 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
     make_dict_nparr = lambda: defaultdict(lambda: np.full((ncols,), -9999, dtype=np.float))
 
     output_grids = {
-        "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
-        #"AbBiom": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "AbBiom": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "Pwp_S1": {"data": make_dict_nparr(), "cast-to": "float", "digits": 3},
+        "Fc_S1": {"data": make_dict_nparr(), "cast-to": "float", "digits": 3}
     }
     output_keys = list(output_grids.keys())
 
