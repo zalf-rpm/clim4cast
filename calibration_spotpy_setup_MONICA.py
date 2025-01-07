65a7893fff8a957900dc3a44310acc9df4dd0c88
OlgaWold <oboyantseva.1996@gmail.com>
2025-01-07 13:36:19 +0100

    percentage diff 5 params

--- calibration_spotpy_setup_MONICA.py
+++ calibration_spotpy_setup_MONICA.py
@@ -119,8 +119,8 @@ class spot_setup(object):
     def objectivefunction(self, simulation, evaluation):
 
         #return unbiased_rmse_RB(evaluation, simulation)
-        return spotpy.objectivefunctions.rmse(evaluation, simulation)
-        #return calculate_percentage_difference_new(evaluation, simulation)
+        #return spotpy.objectivefunctions.rmse(evaluation, simulation)
+        return calculate_percentage_difference_new(evaluation, simulation)
         #return calculate_weighted_rmse(evaluation, simulation, self.weight_per_region)
 
 
